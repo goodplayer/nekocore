d4173d954d212a17879bfe41817042a722855367
goodplayer <woshiyigelaji@gmail.com>
2013-06-11 15:52:11 +0800

    feature: arrayutil add sub array feature: add byte array xor

--- src/main/java/net/moetang/nekocore/lang/ArrayUtil.java
+++ src/main/java/net/moetang/nekocore/lang/ArrayUtil.java
@@ -3,73 +3,106 @@ package net.moetang.nekocore.lang;
 import java.util.Arrays;
 
 public class ArrayUtil {
-	//Do nothing
-	public static void concat(){
+	// Do nothing
+	public static void concat() {
 		FastException.throwIt();
 	}
+
 	@SafeVarargs
 	public final static <T> T[] concat(T[]... arrays) {
 		int n = 0;
 		int i = 0;
 		int pos = 0;
 		int len = arrays.length;
-		for(; i < len; i++){
+		for (; i < len; i++) {
 			n += arrays[i].length;
 		}
-		if(len > 0){
+		if (len > 0) {
 			T[] arr = arrays[0];
 			pos = arr.length;
 			T[] newArray = Arrays.copyOf(arr, n);
-			for(i = 1; i < len ; i++){
+			for (i = 1; i < len; i++) {
 				arr = arrays[i];
-				if(arr.length>0){
+				if (arr.length > 0) {
 					System.arraycopy(arr, 0, newArray, pos, arr.length);
 					pos += arr.length;
 				}
 			}
 			return newArray;
-		}else{
+		} else {
 			return null;
 		}
 	}
-    public final static byte[] clone(byte[] array)
-    {
-        if (array == null)
-        {
-            return null;
-        }
-        byte[] result = new byte[array.length];
-        System.arraycopy(array, 0, result, 0, array.length);
-        return result;
-    }
-    public final static int[] clone(int[] array)
-    {
-        if (array == null)
-        {
-            return null;
-        }
-        int[] result = new int[array.length];
-        System.arraycopy(array, 0, result, 0, array.length);
-        return result;
-    }
-    public final static long[] clone(long[] array)
-    {
-        if (array == null)
-        {
-            return null;
-        }
-        long[] result = new long[array.length];
-        System.arraycopy(array, 0, result, 0, array.length);
-        return result;
-    }
-    public final static short[] clone(short[] array)
-    {
-        if (array == null)
-        {
-            return null;
-        }
-        short[] result = new short[array.length];
-        System.arraycopy(array, 0, result, 0, array.length);
-        return result;
-    }
+
+	public final static byte[] clone(byte[] array) {
+		if (array == null) {
+			return null;
+		}
+		byte[] result = new byte[array.length];
+		System.arraycopy(array, 0, result, 0, array.length);
+		return result;
+	}
+
+	public final static int[] clone(int[] array) {
+		if (array == null) {
+			return null;
+		}
+		int[] result = new int[array.length];
+		System.arraycopy(array, 0, result, 0, array.length);
+		return result;
+	}
+
+	public final static long[] clone(long[] array) {
+		if (array == null) {
+			return null;
+		}
+		long[] result = new long[array.length];
+		System.arraycopy(array, 0, result, 0, array.length);
+		return result;
+	}
+
+	public final static short[] clone(short[] array) {
+		if (array == null) {
+			return null;
+		}
+		short[] result = new short[array.length];
+		System.arraycopy(array, 0, result, 0, array.length);
+		return result;
+	}
+
+	public static byte[] subArray(byte[] input, int start, int end) {
+		byte[] result = new byte[end - start];
+		System.arraycopy(input, start, result, 0, end - start);
+		return result;
+	}
+	public static short[] subArray(short[] input, int start, int end) {
+		short[] result = new short[end - start];
+		System.arraycopy(input, start, result, 0, end - start);
+		return result;
+	}
+	public static int[] subArray(int[] input, int start, int end) {
+		int[] result = new int[end - start];
+		System.arraycopy(input, start, result, 0, end - start);
+		return result;
+	}
+	public static long[] subArray(long[] input, int start, int end) {
+		long[] result = new long[end - start];
+		System.arraycopy(input, start, result, 0, end - start);
+		return result;
+	}
+	public static float[] subArray(float[] input, int start, int end) {
+		float[] result = new float[end - start];
+		System.arraycopy(input, start, result, 0, end - start);
+		return result;
+	}
+	public static double[] subArray(double[] input, int start, int end) {
+		double[] result = new double[end - start];
+		System.arraycopy(input, start, result, 0, end - start);
+		return result;
+	}
+	public static char[] subArray(char[] input, int start, int end) {
+		char[] result = new char[end - start];
+		System.arraycopy(input, start, result, 0, end - start);
+		return result;
+	}
 }
--- src/main/java/net/moetang/nekocore/lang/BytesUtil.java
+++ src/main/java/net/moetang/nekocore/lang/BytesUtil.java
@@ -1,17 +1,18 @@
 package net.moetang.nekocore.lang;
 
 public class BytesUtil {
-	public final static byte[] intToBytes(int src){
+	public final static byte[] intToBytes(int src) {
 		return intToBytes(src, false);
 	}
-	public final static byte[] intToBytes(int src, boolean isBigEndian){
+
+	public final static byte[] intToBytes(int src, boolean isBigEndian) {
 		byte[] r = new byte[4];
-		if(isBigEndian){
+		if (isBigEndian) {
 			r[3] = (byte) (src & 0xff);
 			r[2] = (byte) ((src >> 8) & 0xff);
 			r[1] = (byte) ((src >> 16) & 0xff);
 			r[0] = (byte) ((src >> 24) & 0xff);
-		}else{
+		} else {
 			r[0] = (byte) (src & 0xff);
 			r[1] = (byte) ((src >> 8) & 0xff);
 			r[2] = (byte) ((src >> 16) & 0xff);
@@ -19,12 +20,14 @@ public class BytesUtil {
 		}
 		return r;
 	}
-	public final static byte[] longToBytes(long src){
+
+	public final static byte[] longToBytes(long src) {
 		return longToBytes(src, false);
 	}
-	public final static byte[] longToBytes(long src, boolean isBigEndian){
+
+	public final static byte[] longToBytes(long src, boolean isBigEndian) {
 		byte[] r = new byte[8];
-		if(isBigEndian){
+		if (isBigEndian) {
 			r[7] = (byte) (src & 0xff);
 			r[6] = (byte) ((src >> 8) & 0xff);
 			r[5] = (byte) ((src >> 16) & 0xff);
@@ -33,7 +36,7 @@ public class BytesUtil {
 			r[2] = (byte) ((src >> 40) & 0xff);
 			r[1] = (byte) ((src >> 48) & 0xff);
 			r[0] = (byte) ((src >> 56) & 0xff);
-		}else{
+		} else {
 			r[0] = (byte) (src & 0xff);
 			r[1] = (byte) ((src >> 8) & 0xff);
 			r[2] = (byte) ((src >> 16) & 0xff);
@@ -45,4 +48,13 @@ public class BytesUtil {
 		}
 		return r;
 	}
+
+	public final static byte[] xor(byte[] x1, byte[] x2) {
+		byte[] out = new byte[x1.length];
+		for (int i = 0; i < x1.length; i++) {
+			out[i] = (byte) (x1[i] ^ x2[i]);
+		}
+		return out;
+	}
+
 }
