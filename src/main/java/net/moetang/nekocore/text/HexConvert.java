60019b9672bd2d9297c25d0dccb0111be3eb225d
goodplayer <woshiyigelaji@gmail.com>
2013-06-11 15:09:43 +0800

    feature: add offset to hexconvert

--- src/main/java/net/moetang/nekocore/text/HexConvert.java
+++ src/main/java/net/moetang/nekocore/text/HexConvert.java
@@ -13,8 +13,12 @@ public class HexConvert {
 		return sb.toString();
 	}
 	public final static String toHexFast(byte[] buffer) {
-		StringBuffer sb = new StringBuffer(buffer.length * 2);
-		for (int i = 0; i < buffer.length; i++) {
+		return toHexFast(buffer, 0, buffer.length);
+	}
+	public final static String toHexFast(byte[] buffer, int offset, int len){
+		StringBuffer sb = new StringBuffer(len * 2);
+		int stopPos = offset + len;
+		for (int i = offset; i < stopPos; i++) {
 			sb.append(HEX_CHAR[(buffer[i] & 0xf0) >> 4]);
 			sb.append(HEX_CHAR[buffer[i] & 0x0f]);
 		}
